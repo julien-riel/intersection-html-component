65c3047503367e7bc7bcc565f92102987b1fd11f
Julien Riel <julien.riel@gmail.com>
2021-12-04 00:37:07 -0500

    remove js beside ts

--- html/scripts/config.js
+++
@@ -1,7 +0,0 @@
-//      const svgns = "http://www.w3.org/2000/svg";
-//   const racineSvg = "street";
-const couleurRoute = "gray";
-const distanceLigneArret = 20;
-const distanceFlechesLigneArret = 100;
-const defaultLaneWidth = 100;
-export { distanceLigneArret, distanceFlechesLigneArret, couleurRoute, defaultLaneWidth };
